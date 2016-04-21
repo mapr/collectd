454b54f5fcffc3836ef1989970c1816a5304e0e0
ntirupattur@maprtech.com <ntirupattur@maprtech.com>
2016-04-21 00:17:37 -0700

    #SPYG-371 Unable to fetch metrics for mapr_process.ps_cpupercent

--- src/mapr_process.c
+++ src/mapr_process.c
@@ -790,7 +790,7 @@ sys_cpu_user_nice_counter + sys_cpu_system_counter +
 sys_cpu_idle_counter;
 ss->sys_boot_time_secs = si.uptime;
 ss->sys_tot_phys_mem = sys_tot_phys_mem;
-//ss->sys_boot_time_secs = time(NULL) - sys_boot_time_secs;
+ss->sys_boot_time_secs = time(NULL) - sys_boot_time_secs;
 INFO ("%s sys u:%llu n:%llu s:%llu i:%llu physmem: %llu, boottime: %lu\n",
 		name, sys_cpu_user_counter, sys_cpu_user_nice_counter,
 		sys_cpu_system_counter, sys_cpu_idle_counter, sys_tot_phys_mem,
