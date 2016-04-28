287ae72156214bf880d70bf1f219d1bd1a89c49a
ntirupattur@maprtech.com <ntirupattur@maprtech.com>
2016-04-28 14:26:54 -0700

    #SPYG-371 Unable to fetch metrics for mapr_process.ps_cpupercent

--- src/mapr_process.c
+++ src/mapr_process.c
@@ -1112,8 +1112,7 @@ static void ps_calc_cpu_percent(sysstat_t *ss, sysstat_t *prev_ss, procstat_t *p
     float et;
 
     gettimeofday(&timev, &timez);
-    et = (timev.tv_sec - oldtimev.tv_sec)
-                 + (float)(timev.tv_usec - oldtimev.tv_usec) / 1000000.0;
+    et = (timev.tv_sec - oldtimev.tv_sec) + (float)((timev.tv_usec - oldtimev.tv_usec) / 1000000.0);
     oldtimev.tv_sec = timev.tv_sec;
     oldtimev.tv_usec = timev.tv_usec;
 
@@ -1121,8 +1120,8 @@ static void ps_calc_cpu_percent(sysstat_t *ss, sysstat_t *prev_ss, procstat_t *p
     double cpu_percent;
     ps_find_cpu_delta(ps, &ps_cpu_user_delta, &ps_cpu_system_delta);
     ss_cpu_tot_time_delta = ss->sys_cpu_tot_time_counter - prev_ss->sys_cpu_tot_time_counter;
-    //cpu_percent = (ps_cpu_system_delta + ps_cpu_user_delta) * 100.0 / ss_cpu_tot_time_delta;
-    cpu_percent = (ps_cpu_system_delta + ps_cpu_user_delta) * 100.0 / et ;
+    cpu_percent = (ps_cpu_system_delta + ps_cpu_user_delta) * 100.0 / ss_cpu_tot_time_delta;
+    //cpu_percent = (ps_cpu_system_delta + ps_cpu_user_delta) * 100.0 / et ;
 
     INFO ("%s proc with %lu pid delta: u: %lu, s: %lu, tot: %lu, percent: %f\n", ps->name, ps->pid,ps_cpu_user_delta, ps_cpu_system_delta, ss_cpu_tot_time_delta,cpu_percent);
     ps->cpu_percent = cpu_percent;
