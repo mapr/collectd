b9c9a85df146a7c9c4a8987f43f078397cb31098
larsf <lfredriksen@maprtech.com>
2017-01-31 13:22:17 -0800

    fix here doc

--- ext-conf/install_devtools.sh
+++ ext-conf/install_devtools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x 
-MEPVER={-:3.0.0}
+MEPVER=${MAPRVER:-3.0.0}
 MEPREPO=http://artifactory.devops.lab/artifactory/prestage/releases-dev/MEP/MEP-${MEPVER}
 
 function checkerror() {
@@ -12,14 +12,14 @@ function checkerror() {
 
 
 if [ -f /etc/redhat-release ]; then
-    cat > /etc/yum.repos.d/mapr_mep.list  <<-EOF
+    cat > /etc/yum.repos.d/mapr_mep.repo <<EOF
 [MapR_Ecosystem]
 name = MapR Ecosystem Components
 baseurl = $MEPREPO/redhat
 gpgcheck = 0
 enabled = 1
 protected = 1
-EOF 
+EOF
 
     checkerror "Failed to install MEP repo"
     yum clean all
@@ -30,9 +30,9 @@ EOF
     rpm -i --nodeps /tmp/cache/mapr-librdkafka*
     checkerror "Failed to install mapr-librdkafka"
 else
-    echo > /etc/apt/sources.list.d/mapr_mep.list <<-EOF
+    echo > /etc/apt/sources.list.d/mapr_mep.list <<EOR
 deb $MEPREPO/ubuntu binary trusty
-EOF
+EOR
     checkerror "Failed to install MEP repo"
     apt-get update
     apt-get -y --nodeps install mapr-libkafka
